c85694bf7f40dfb0ce14c81552ac168b9585f23f
David Flenner <david.flenner@gmail.com>
2021-08-15 14:21:10 -0400

    Added parametrized paths

--- applets/js/MathFunctions.js
+++ applets/js/MathFunctions.js
@@ -2347,82 +2347,91 @@ function evaluate(f, x, args) {
 		args = {};
 	}
 
-	var variable = args.variable ? args.variable : 'x';
+	if(x !== undefined) {
 
-	f = f.toLowerCase();
+		var variable = args.variable ? args.variable : 'x';
 
-	// Make sure to ignore points and asymptotes
-	if (f.includes(';') || isPoint(f) || isAsymptote(f)) {
-		var newf = '';
-		f_list = f.split(';');
-		for(var i = 0; i < f_list.length; i++) {
-			if (!isPoint(f_list[i]) && !isAsymptote(f_list[i])) {
-				newf += f_list[i] + ';'
-			}
-		}
-		f = newf.substring(0, newf.length - 1);
-	}
+		f = f.toLowerCase();
 
-	if(f.search(regex_hole) != -1) {
-		var interval = getInterval(f);
-		var xloc = getHoleValue(interval);
-		if(x == xloc) {
-			return NaN;
-		}
-		f = removeInterval(f);
-	}
-
-	// if f includes a restricted interval, handle that
-	if(f.search(regex_interval) != -1) {
-
-		x_on_interval = false;
-
-		if(f.includes(';')) {
+		// Make sure to ignore points and asymptotes
+		if (f.includes(';') || isPoint(f) || isAsymptote(f)) {
+			var newf = '';
 			f_list = f.split(';');
-		} else {
-			f_list = [f];
-		}
-		for (var i = 0; i < f_list.length; i++) {
-			var loc = f_list[i].search(regex_interval);
-			if(loc != -1) {
-				interval = f_list[i].substring(loc, f_list[i].length);
-				lowerbound = getLowerEndpoint(interval);
-				upperbound = getUpperEndpoint(interval);
-				var upperboundclosed = !upperBoundOpen(interval);
-				var lowerboundclosed = !lowerBoundOpen(interval);
-				if((x > lowerbound) && (x < upperbound)) {
-					f = f_list[i].substring(0, loc);
-					x_on_interval = true;
-				}
-				if((x == lowerbound) && lowerboundclosed) {
-					f = f_list[i].substring(0, loc);
-					x_on_interval = true;
-				}
-				if((x == upperbound) && upperboundclosed) {
-					f = f_list[i].substring(0, loc);
-					x_on_interval = true;
+			for(var i = 0; i < f_list.length; i++) {
+				if (!isPoint(f_list[i]) && !isAsymptote(f_list[i])) {
+					newf += f_list[i] + ';'
 				}
 			}
+			f = newf.substring(0, newf.length - 1);
 		}
-		if(!x_on_interval) {
-			//f = NaN;
+
+		if(f.search(regex_hole) != -1) {
+			var interval = getInterval(f);
+			var xloc = getHoleValue(interval);
+			if(x == xloc) {
+				return NaN;
+			}
+			f = removeInterval(f);
+		}
+
+		// if f includes a restricted interval, handle that
+		if(f.search(regex_interval) != -1) {
+
+			x_on_interval = false;
+
+			if(f.includes(';')) {
+				f_list = f.split(';');
+			} else {
+				f_list = [f];
+			}
+			for (var i = 0; i < f_list.length; i++) {
+				var loc = f_list[i].search(regex_interval);
+				if(loc != -1) {
+					interval = f_list[i].substring(loc, f_list[i].length);
+					lowerbound = getLowerEndpoint(interval);
+					upperbound = getUpperEndpoint(interval);
+					var upperboundclosed = !upperBoundOpen(interval);
+					var lowerboundclosed = !lowerBoundOpen(interval);
+					if((x > lowerbound) && (x < upperbound)) {
+						f = f_list[i].substring(0, loc);
+						x_on_interval = true;
+					}
+					if((x == lowerbound) && lowerboundclosed) {
+						f = f_list[i].substring(0, loc);
+						x_on_interval = true;
+					}
+					if((x == upperbound) && upperboundclosed) {
+						f = f_list[i].substring(0, loc);
+						x_on_interval = true;
+					}
+				}
+			}
+			if(!x_on_interval) {
+				//f = NaN;
+				return NaN;
+			}
+		}
+
+		// math.js doesn't support ln
+		f = f.replace(/ln/g, "log");
+
+		var expr = math.compile(f);
+		var parameter = {};
+		parameter[variable] = x;
+
+		// math.js returns complex numbers for negative values of logs, etc. This
+		// code below makes sure to only return real numbers
+		try {
+			var val = expr.eval(parameter); // An older version of mathjs used eval instead of evaluate
+		} catch (TypeError) {
+			var val = expr.evaluate(parameter);
+		}
+		if (typeof(val) == 'object') {
 			return NaN;
-		}
-	}
-
-	// math.js doesn't support ln
-	f = f.replace(/ln/g, "log");
-
-	var expr = math.compile(f);
-	var parameter = {};
-	parameter[variable] = x;
-
-	// math.js returns complex numbers for negative values of logs, etc. This
-	// code below makes sure to only return real numbers
-	var val = expr.eval(parameter);
-	if (typeof(val) == 'object') {
-		return NaN;
-	} else return val;
+		} else return parseFloat(val);
+	} else {
+	 	return parseFloat(math.evaluate(f));
+ 	}
 
 }
 
