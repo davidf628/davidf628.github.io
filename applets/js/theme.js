ac888fcb84c846096d1dd00baac0217b2390f3c3
davidf628 <30780186+davidf628@users.noreply.github.com>
2025-02-16 16:00:22 -0500

    more work on themes
    created the ability to change the theme of the board more easily

--- applets/js/theme.js
+++ applets/js/theme.js
@@ -1,17 +1,73 @@
 function set_darkmode(board) {
-    //board.renderer.container.style.backgroundColor = 'black';
+
         // Change board background color dynamically
-        document.getElementById('jxgbox').style.backgroundColor = '#1e1e1e';
+        board.renderer.container.style.backgroundColor = '#2e2e2b';
 
         // Change grid and axis colors
-        board.defaultAxes.x.setAttribute({ strokeColor: '#bbbbbb' });
-        board.defaultAxes.y.setAttribute({ strokeColor: '#bbbbbb' });
-        board.renderer.container.style.color = '#ffffff'; // Text color
-    
-        // Change color of all existing elements
-        board.objectsList.forEach(obj => {
-            obj.setAttribute({ strokeColor: '#ffffff', fillColor: '#888' });
+        board.defaultAxes.x.setAttribute({ 
+            strokeColor: '#ffffff',
+            highlightStrokeColor: '#ffffff',
         });
+        board.defaultAxes.y.setAttribute({ 
+            strokeColor: '#ffffff',
+            highlightStrokeColor: '#ffffff',
+        });
+        board.defaultAxes.x.defaultTicks.setAttribute({
+            strokeColor: '#ffffff',
+            highlightStrokeColor: '#ffffff',
+            label: {
+                strokeColor: '#ffffff',
+                highlightStrokeColor: '#ffffff',
+            }
+        });
+        board.defaultAxes.y.defaultTicks.setAttribute({
+            strokeColor: '#ffffff',
+            highlightStrokeColor: '#ffffff',
+            label: {
+                strokeColor: '#ffffff',
+                highlightStrokeColor: '#ffffff',
+            }
+        });
+        // Change color of all existing elements
+     /*   board.objectsList.forEach(obj => {
+            obj.setAttribute({ strokeColor: '#ffffff', fillColor: '#888' });
+        });*/
+}
+
+function set_lightmode(board) {
+
+    // Change board background color dynamically
+    board.renderer.container.style.backgroundColor = '#ffffff';
+
+    // Change grid and axis colors
+    board.defaultAxes.x.setAttribute({ 
+        strokeColor: '#666666',
+        highlightStrokeColor: '#888888',
+    });
+    board.defaultAxes.y.setAttribute({ 
+        strokeColor: '#666666',
+        highlightStrokeColor: '#888888',
+    });
+    board.defaultAxes.x.defaultTicks.setAttribute({
+        strokeColor: '#666666',
+        highlightStrokeColor: '#888888',
+        label: {
+            strokeColor: '#666666',
+            highlightStrokeColor: '#888888',
+        }
+    });
+    board.defaultAxes.y.defaultTicks.setAttribute({
+        strokeColor: '#666666',
+        highlightStrokeColor: '#888888',
+        label: {
+            strokeColor: '#666666',
+            highlightStrokeColor: '#888888',
+        }
+    });
+    // Change color of all existing elements
+   /* board.objectsList.forEach(obj => {
+        obj.setAttribute({ strokeColor: '#ffffff', fillColor: '#888' });
+    });*/
 }
 
 // Toggle between dark and light mode
@@ -20,6 +76,78 @@ function toggleDarkMode(board) {
     if (isDark) {
         set_darkmode(board);
     } else {
-        location.reload(); // Reload to reset
+        set_lightmode(board); 
     }
+}
+
+function setTheme(board, theme) {
+
+    // Change board background color dynamically
+    board.renderer.container.style.backgroundColor = theme.board.backgroundColor;
+
+    // Change grid and axis colors
+    board.defaultAxes.x.setAttribute({ 
+        strokeColor: theme.xaxis.strokeColor,
+        highlightStrokeColor: theme.xaxis.highlightStrokeColor,
+    });
+    board.defaultAxes.y.setAttribute({ 
+        strokeColor: theme.yaxis.strokeColor,
+        highlightStrokeColor: theme.yaxis.highlightStrokeColor,
+    });
+    board.defaultAxes.x.defaultTicks.setAttribute({
+        strokeColor: theme.xaxis.strokeColor,
+        highlightStrokeColor: theme.xaxis.highlightStrokeColor,
+        label: {
+            strokeColor: theme.xaxis.label.strokeColor,
+            highlightStrokeColor: theme.xaxis.label.highlightStrokeColor,
+        }
+    });
+    board.defaultAxes.y.defaultTicks.setAttribute({
+        strokeColor: theme.yaxis.strokeColor,
+        highlightStrokeColor: theme.yaxis.highlightStrokeColor,
+        label: {
+            strokeColor: theme.yaxis.label.strokeColor,
+            highlightStrokeColor: theme.yaxis.label.highlightStrokeColor,
+        }
+    });
+    // Change color of all existing elements
+   /* board.objectsList.forEach(obj => {
+        obj.setAttribute({ strokeColor: '#ffffff', fillColor: '#888' });
+    });*/
+}
+
+function getCurrentTheme(board) {
+
+    theme = {
+        board: {
+            backgroundColor: board.renderer.container.style.backgroundColor,
+        },
+        xaxis: {
+            strokeColor: board.defaultAxes.x.getAttribute('strokeColor'),
+            highlightStrokeColor: board.defaultAxes.x.getAttribute('highlightStrokeColor'),
+            ticks: {
+                strokeColor: board.defaultAxes.x.defaultTicks.getAttribute('strokeColor'),
+                highlightStrokeColor: board.defaultAxes.x.defaultTicks.getAttribute('highlightStrokeColor'),
+                label: {
+                    strokeColor: board.defaultAxes.x.defaultTicks.getAttribute('label').strokecolor,
+                    highlightStrokeColor: board.defaultAxes.x.defaultTicks.getAttribute('label').highlightstrokecolor,
+                }
+            }
+        },
+        yaxis: {
+            strokeColor: board.defaultAxes.y.getAttribute('strokeColor'),
+            highlightStrokeColor: board.defaultAxes.y.getAttribute('highlightStrokeColor'),
+            ticks: {
+                strokeColor: board.defaultAxes.y.defaultTicks.getAttribute('strokeColor'),
+                highlightStrokeColor: board.defaultAxes.y.defaultTicks.getAttribute('highlightStrokeColor'),
+                label: {
+                    strokeColor: board.defaultAxes.y.defaultTicks.getAttribute('label').strokecolor,
+                    highlightStrokeColor: board.defaultAxes.y.defaultTicks.getAttribute('label').highlightstrokecolor,
+                }
+            }
+        }
+    }
+
+    return theme;
+
 }
